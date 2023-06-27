5489514ee7edccbbb668087b6127395e7386f457
NoMad42 <nomad42@nomad42.ru>
2023-06-28 01:17:49 +0300

    remove unused cache package

--- src/main/kotlin/ru/nomad42/playzone/features/cache/InMemoryCache.kt
+++
@@ -1,8 +0,0 @@
-package ru.nomad42.playzone.features.cache
-
-import ru.nomad42.playzone.features.register.RegisterRemoteRequest
-
-object InMemoryCache {
-    val userList: MutableList<RegisterRemoteRequest> = mutableListOf()
-    val token: MutableList<TokenCache> = mutableListOf()
-}
--- src/main/kotlin/ru/nomad42/playzone/features/cache/TokenCache.kt
+++
@@ -1,6 +0,0 @@
-package ru.nomad42.playzone.features.cache
-
-data class TokenCache(
-    val login: String,
-    val token: String
-)
